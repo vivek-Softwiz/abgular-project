671892586508a8d5a54120c3c5d553b82b20caba
vivek ranjan <vivek.softwiz@gmail.com>
2021-07-10 01:43:45 +0530

    njncvfvf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComComponent } from './com/com.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComComponent
   ],
   imports: [
     BrowserModule,
